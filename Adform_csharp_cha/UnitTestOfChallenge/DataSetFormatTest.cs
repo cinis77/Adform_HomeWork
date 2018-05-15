c57ec8780180f0404047b776f3236f345f66b7ef
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-05-15 09:41:13 +0300

    Unit test upload

---
+++ Adform_csharp_cha/UnitTestOfChallenge/DataSetFormatTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Adform_csharp_cha;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOfChallenge
+{
+    [TestClass]
+    public class DataSetFormatTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof (FormatException))]
+        public void FormDataFromString_DateAndCountAreMixed_ThowsAnException()
+        {
+            DataSetFormat dataSetForTesting = new DataSetFormat();
+            dataSetForTesting.FormDataFromString(new string[] { "120", "2018.01.01" }); 
+        }
+
+        [TestMethod]
+        public void FormDataFromString_DateAndCountAreCorrect_ShouldConvertString()
+        {
+            DataSetFormat dataSetForTesting = new DataSetFormat();
+            dataSetForTesting.FormDataFromString(new string[] {  "2018.01.01", "120" });
+            DateTime TestDate = new DateTime(year: 2018, month: 01, day: 01);
+            Assert.AreEqual(TestDate, dataSetForTesting.DateOfData);
+            Assert.AreEqual(120, dataSetForTesting.DataAmount);
+
+        }
+    }
+}
---
+++ Adform_csharp_cha/UnitTestOfChallenge/JsonStringCreatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Adform_csharp_cha;
+using Newtonsoft.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOfChallenge
+{
+    [TestClass]
+    public class JsonStringCreatorTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void CreateJsonString_NullObjectSend_ThowsException()
+        {
+            FakeFilter fakeFilter = new FakeFilter(null);
+            JsonStringCreator jsCreator = new JsonStringCreator(fakeFilter, new string[] { },new string[] { });
+            jsCreator.CreateJsonString(null);
+        }
+
+     }
+
+
+    public class FakeFilter : IFilterTypeData
+    {
+        public FilterDate date { get; }
+
+        public FakeFilter(FilterDate date)
+        {
+            this.date = date;
+        }
+    }
+}
